136da5b5a9f140689cfd71b41754aad38d1acb75
Roman Frołow <rofrol@gmail.com>
2021-07-29 18:15:53 +0200

    25 and 5

--- public/main.js
+++ public/main.js
@@ -14,6 +14,7 @@ let interval;
 let timer = 0;
 
 function startTimer(duration, display, callback) {
+  clearInterval(interval);
   if (timer === 0) timer = duration;
   startpause.textContent = "Pause";
   startpause.onclick = pauseTimer;
@@ -45,15 +46,28 @@ const pauseTimer = () => {
 const resetTimer = () => {
   clearInterval(interval);
   startpause.textContent = "Start";
-  startpause.onclick = () => startTimer(seconds, display, notification);
+  startpause.onclick = () => startTimer(pomodoroTime, display, notification);
   timer = 0;
-  display.textContent = formatSeconds(seconds);
+  display.textContent = formatSeconds(pomodoroTime);
 };
 
-const seconds = 60 * 25;
+const startPomodoro = () => {
+  timer = 0;
+  startTimer(pomodoroTime, display, notification);
+};
+
+const startBreak = () => {
+  timer = 0;
+  startTimer(breakTime, display, notification);
+};
+
+const pomodoroTime = 60 * 25;
+const breakTime = 60 * 5;
 const display = document.querySelector("#time");
 const startpause = document.querySelector("#startpause");
-window.startTimer = () => startTimer(seconds, display, notification);
+window.startTimer = startPomodoro;
 window.pauseTimer = pauseTimer;
 window.resetTimer = resetTimer;
 window.interval = interval;
+window.startPomodoro = startPomodoro;
+window.startBreak = startBreak;
